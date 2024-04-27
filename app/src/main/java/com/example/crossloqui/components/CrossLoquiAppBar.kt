1d879eea7e89c210b9fdc92eda851ce22d74f5c9
longzhen <koreabnl@gmail.com>
2024-04-28 00:49:02 +0900

    build a simple AppBar

---
+++ app/src/main/java/com/example/crossloqui/components/CrossLoquiAppBar.kt
@@ -0,0 +1,52 @@
+package com.example.crossloqui.components
+
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.crossloqui.ui.theme.CrossLoquiTheme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CrossLoquiAppBar(
+    modifier: Modifier = Modifier,
+    scrollBehavior: TopAppBarScrollBehavior? = null,
+    title: @Composable () -> Unit,
+    actions: @Composable RowScope.() -> Unit = {}
+) {
+    TopAppBar(
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            titleContentColor = MaterialTheme.colorScheme.primary
+        ),
+        title = title,
+        scrollBehavior = scrollBehavior,
+        actions = actions,
+        modifier = modifier
+    )
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview
+@Composable
+fun CrossLoquiAppBarPreview() {
+    CrossLoquiTheme {
+        CrossLoquiAppBar(title = { Text(text = "AppBar") })
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview
+@Composable
+fun CrossLoquiviAppBarPreviewDark() {
+    CrossLoquiTheme(darkTheme = true) {
+        CrossLoquiAppBar(title = { Text(text = "AppBar") })
+    }
+}
