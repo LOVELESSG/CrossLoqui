a50e6bed168cf5cb494f9869c0cf45c82361862e
longzhen <koreabnl@gmail.com>
2024-11-24 01:05:55 +0900

    Synchronous code

--- app/src/main/java/com/example/crossloqui/firebase/repositories/FirestoreRepository.kt
+++ app/src/main/java/com/example/crossloqui/firebase/repositories/FirestoreRepository.kt
@@ -1,2 +1,15 @@
 package com.example.crossloqui.firebase.repositories
 
+import com.google.android.gms.tasks.Task
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.QuerySnapshot
+
+class FirestoreRepository {
+    val firestoreDB = FirebaseFirestore.getInstance()
+
+    suspend fun getNewFriendList(currentUserId: String): Task<QuerySnapshot> {
+        return firestoreDB.collection("friendRequest")
+            .whereArrayContains("members", currentUserId)
+            .get()
+    }
+}
